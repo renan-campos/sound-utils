c508e8b2f9ed69cfd28c9243d2d0989783192c72
Renan Campos <rcampos@redhat.com>
2022-06-06 20:52:28 -0400

    Updated logging system

--- cmd/playWav.go
+++ cmd/playWav.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	. "github.com/renan-campos/sound-utils/pkg/logging"
+	"github.com/renan-campos/sound-utils/pkg/logging"
 
 	"github.com/pkg/errors"
 	"github.com/renan-campos/sound-utils/pkg/alsa"
@@ -17,9 +17,12 @@ func usage() string {
 }
 
 func main() {
+
+	logging.DisplayDebug = true
+
 	if len(os.Args) < 2 {
-		Stderr("Insufficient number of arguments")
-		Stderr(usage())
+		logging.Stderr("Insufficient number of arguments")
+		logging.Stderr(usage())
 		os.Exit(1)
 	}
 
@@ -31,20 +34,20 @@ func main() {
 	card, err := alsa.FindCard(cardName)
 	defer alsa.CloseCard(card)
 	if err != nil {
-		Stderr(errors.Wrap(err, "Failed to find card").Error())
+		logging.Stderr(errors.Wrap(err, "Failed to find card").Error())
 		os.Exit(1)
 	}
-	fmt.Println(card, "found!")
+	logging.Debugf("%s found.\n", card)
 
 	device, err := alsa.FindPlayableDevice(card, deviceName)
 	if err != nil {
-		Stderr(errors.Wrap(err, "Failed to determine playable device").Error())
+		logging.Stderr(errors.Wrap(err, "Failed to determine playable device").Error())
 		os.Exit(1)
 	}
-	fmt.Println("  ", device, "found!")
+	logging.Debugf("%s found.\n", device)
 
 	if err := alsa.PlayWav(device, wavFileName); err != nil {
-		Stderr(errors.Wrap(err, "failed to play wav file on device").Error())
+		logging.Stderr(errors.Wrap(err, "failed to play wav file on device").Error())
 		os.Exit(1)
 	}
 }
--- pkg/logging/output.go
+++ pkg/logging/output.go
@@ -8,3 +8,11 @@ import (
 func Stderr(format string, a ...interface{}) {
 	fmt.Fprintf(os.Stderr, "Error: "+format+"\n", a...)
 }
+
+var DisplayDebug bool
+
+func Debugf(format string, a ...interface{}) {
+	if DisplayDebug {
+		fmt.Fprintf(os.Stdout, format, a...)
+	}
+}
